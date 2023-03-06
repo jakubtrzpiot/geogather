1513b8c84a4736496373e31268999f539e1fb11d
jakubtrzpiot <j.trzpiot03@gmail.com>
2023-03-06 02:16:42 +0100

    fix home screen merge conflicts

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -3,58 +3,82 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen ({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.buttonContainer}>
-            <Pressable
-                style={styles.button}
-                title="GeoGather"
-                onPress={() => navigation.navigate('Map')}
-            >
-                <Text style={styles.text}>Map</Text>
-            </Pressable>
-            <Pressable
-                style={styles.button}
-                onPress={() => navigation.navigate('Profile')}
-            >
-                <Text style={styles.text}>Profile</Text>
-            </Pressable>
-            </View>
-        </View>
-    );
+function HomeScreen({ navigation }) {
+  const events = [
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+    { time: '9:00 PM', date: '01.01.2023', description: 'test', iconName: '' },
+  ];
+  return (
+    <View style={styles.container}>
+      <ScrollView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        {events.map(({ time, date, description, iconName }, i) => (
+          <EventTile
+            key={i}
+            time={time}
+            date={date}
+            description={description}
+            iconName={iconName}
+          />
+        ))}
+      </ScrollView>
+
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={styles.button}
+          title="GeoGather"
+          onPress={() => navigation.navigate('Map')}
+        >
+          <Text style={styles.text}>Map</Text>
+        </Pressable>
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate('Profile')}
+        >
+          <Text style={styles.text}>Profile</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        width: '100%',
-        elevation: 3,
-        padding: 10,
-    },
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: 'white',
-    },
-    text: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'black',
-      },
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    width: '100%',
+    elevation: 3,
+    padding: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+  },
+});
 
 export default HomeScreen;
